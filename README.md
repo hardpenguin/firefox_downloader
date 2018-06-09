924b5163d9716e37695f7f481657c2672bfac7f6
hardpenguin <hardpenguin13@gmail.com>
2018-06-09 08:52:18 +0200

    init

---
+++ README.md
@@ -0,0 +1,18 @@
+## Description
+
+This script is meant to download the latest stable version of Firefox for Linux, then unpack it into the system-wide path.
+
+It's especially useful in distributions such as Debian Stable that currently don't have a way to easily install and/or update Firefox.
+
+## Status
+
+- untested(!)
+- basic functionality is implemented
+
+## Todo
+
+- alternatives system support
+- some safeguards
+- debug mode
+- support for non-root users
+- parameters support (e.g. language, temp path)
---
+++ firefox_downloader.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# this script is meant to download latest version of Firefox for Linux
+# then unpack it in the system-wide path
+# to put Firefox in $PATH, alternatives system is recommended
+
+ScriptName="basename $0"
+Version="0.1"
+
+FirefoxLink="https://download.mozilla.org/?product=firefox-latest-ssl&os=linux64&lang=pl"
+PackageFileName="firefox.tar.bz2"
+TargetPath="/usr/lib"
+FirefoxDirectory="firefox"
+TempPath="/tmp/firefox_update_$RANDOM"
+
+echo "$ScriptName, version $Version"
+
+if [ "$(whoami)" != "root" ];
+then
+    echo "You are not root."
+    exit 1
+fi
+
+if [ -e "$TargetPath/$FirefoxDirectory" ];
+then
+    echo "Removing previous version..."
+    rm -r "$TargetPath/$FirefoxDirectory"
+fi
+
+echo "Downloading..."
+wget -q "$FirefoxLink" -O "$TempPath/$PackageFileName"
+
+echo "Unpacking..."
+tar xf "$TempPath/$PackageFileName" -C "$TargetPath"
+
+echo "Updating ownership..."
+chown -R root:root "$TargetPath/$FirefoxDirectory"
+
+echo "Done."
+exit 0
